77199a13a92dd2d938a08159fb08be9cd990e0d9
scud119 <39138062+scud119@users.noreply.github.com>
2022-12-22 21:24:01 -0700

    Update gui encrypt.py

--- gui encrypt.py
+++ gui encrypt.py
@@ -13,20 +13,20 @@ def load_key():
     with open("key.key", "rb") as filekey:
         key = filekey.read()
     fernet = Fernet(key)
-    with open(values("-IN-"), "rb") as file:
+    with open(values["-IN-"], "rb") as file:
         original = file.read()
     encrypted = fernet.encrypt(original)
-    with open(values("-IN-"), "wb") as encrypted_file:
+    with open(values["-IN-"], "wb") as encrypted_file:
         encrypted_file.write(encrypted)
 
 def decrypt():
     with open("key.key", "rb") as filekey:
         key = filekey.read()
     fernet = Fernet(key)
-    with open(values("-IN-"), "rb") as enc_file:
+    with open(values["-IN-"], "rb") as enc_file:
         encrypted = enc_file.read()
     decrypted = fernet.decrypt(encrypted)
-    with open(values("-IN-"), "wb") as dec_file:
+    with open(values["-IN-"], "wb") as dec_file:
         dec_file.write(decrypted)
 
 write_key()
